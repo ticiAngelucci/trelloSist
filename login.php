005b47e3d737808303c0216ac8785658db5cb8bf
ticiAngelucci <tittiangelucci08@gmail.com>
2023-10-28 17:44:33 -0300

    button ingresar

--- login.php
+++ login.php
@@ -34,8 +34,9 @@
                     <input type="password" class="form-control" name="password" required>
                 </div>
 
-                <button type="submit" name="ingresar" class="btn btn-primary">Submit</button>
+                <button type="submit" name="ingresar" class="btn btn-primary">Ingresar</button>
             </form>
+            <button class="btn btn-secondary mt-3" onclick="location.href='signUp.php'">¿No tienes cuenta?</button>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
